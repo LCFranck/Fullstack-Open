22e7cb775a7500f4016602fcafb3f13603b7d49d
LCFranck <cassandra.franck@gmail.com>
2025-06-03 22:52:23 +0300

    1.5

--- part1/1_1/src/App.jsx
+++ part1/1_1/src/App.jsx
@@ -39,30 +39,31 @@ const Total = (props) => {
 }
 
 
-
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+   const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header courseName={course} />
-      <Content parts={parts} />
-      <Total total={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
     </div>
   )
 }
