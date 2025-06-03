8158717d0c6c32099cd5acb06fdd4636b7c01b86
LCFranck <cassandra.franck@gmail.com>
2025-06-03 15:53:46 +0300

    assignment 1.1

---
+++ part1/1_1/src/App.jsx
@@ -0,0 +1,53 @@
+
+const Header = (props) => {
+    console.log("header: ", props)
+    return (
+        <div>
+            <h1>{props.courseName}</h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    console.log("content: ", props)
+    return (
+        <div> 
+            <p>{props.parts[0].part} {props.parts[0].exercises} </p>
+            <p>{props.parts[1].part} {props.parts[1].exercises} </p>
+            <p>{props.parts[2].part} {props.parts[2].exercises}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    console.log("total: ", props)
+    return (   
+        <div>
+          <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
+        </div>
+        )
+}
+
+
+
+const App = () => {
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { part: 'Fundamentals of React', exercises: 10 },
+      { part: 'Using props to pass data', exercises: 7 },
+      { part: 'State of a component', exercises: 14 }
+    ]
+  }
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
+    </div>
+  )
+}
+
+export default App;
