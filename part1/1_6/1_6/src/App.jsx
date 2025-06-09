65a5e7de749c6f7009182d205278cf25f5c1ed6a
LCFranck <cassandra.franck@gmail.com>
2025-06-09 19:35:36 +0300

    done with 6-11

--- part1/1_6/1_6/src/App.jsx
+++ part1/1_6/1_6/src/App.jsx
@@ -3,19 +3,58 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistics = (props) => {
-    return (   
-        <div>
-          <p> {props.text}  {props.statistics} </p>
-        </div>
-
+const StatisticLine = (props) => {
+  return (   
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.statistics}</td>
+        </tr>
         )
+
+
 }
+
+
+ 
+const Statistics = (props) => {
+    console.log('Total now', props.total)
+
+      if (props.total === 0) {    
+      return (
+        <div>
+          <p>No feedback given</p>
+       </div>
+       )  
+
+    }
+
+    return (
+        <table>
+          <tbody>
+          <StatisticLine statistics = {props.good} text = {"good"}/>
+          <StatisticLine statistics = {props.neutral} text = {"neutral"}/>
+          <StatisticLine statistics = {props.bad} text = {"bad"} />
+          <StatisticLine statistics = {props.total} text = {"total"} />
+          <StatisticLine statistics = {props.avg} text = {"average"} />
+          <StatisticLine statistics = {props.pos + ' %'} text = {"positive"} /> 
+          </tbody>
+       </table>
+       )  
+    
+}
+
+
 
 const App = (props) => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + bad + neutral;
+  const avg = (good + (bad*-1) + (neutral*0))/total;
+  const pos = (good)/total;
+
+
+
 
   const handleGood = () => {
     console.log('value now', good)
@@ -40,12 +79,19 @@ const App = (props) => {
       <Button onClick={handleNeutral} text="neutral" />
       <Button onClick={handleBad} text="bad" />
       <h1>{"statistics"}</h1>
+          
 
-      <Statistics statistics = {good} text = {"good"}/>
-      <Statistics statistics = {neutral} text = {"neutral"}/>
-      <Statistics statistics = {bad} text = {"bad"} />
+    <Statistics 
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            avg={avg}
+            pos={pos}
+          />
 
     </div>
+   
   )
 }
 
