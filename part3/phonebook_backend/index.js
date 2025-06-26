f60bb465fa47fd2fc8cc95af1f6a7725cc0185ca
LCFranck <cassandra.franck@gmail.com>
2025-06-26 14:47:09 +0300

    3.5 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -27,14 +27,80 @@ let persons = [
     }
 ]
 
+app.use(express.json())
+
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
+  
   response.json(persons)
 })
+
+
+app.get('/api/info', (request, response) => {
+  const time = new Date()
+  const length = persons.length
+
+  const html = `
+    <p> Phonebook has info for ${length} people </p>
+    <p> ${time} </p>`
+    response.send(html)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const randomID = Math.floor(Math.random() * 1000);
+  return String(randomID)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing',
+    })
+  }
+
+  else if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing',
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
