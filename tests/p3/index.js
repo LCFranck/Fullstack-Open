796654191639633fa6d88287bdc908d462a555fb
LCFranck <cassandra.franck@gmail.com>
2025-06-26 14:18:35 +0300

    3.1 done

--- tests/p3/index.js
+++ tests/p3/index.js
@@ -1,30 +1,25 @@
-const http = require('http')
-
 const express = require('express')
 const app = express()
 
-
-
 let notes = [
   {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+    id: '1',
+    content: 'HTML is easy',
+    important: true,
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    id: '2',
+    content: 'Browser can execute only JavaScript',
+    important: false,
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
 ]
 
-
-
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -36,8 +31,7 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
+  const note = notes.find((note) => note.id === id)
 
   if (note) {
     response.json(note)
@@ -46,9 +40,35 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  notes = notes.filter((note) => note.id !== id)
 
   response.status(204).end()
 })
