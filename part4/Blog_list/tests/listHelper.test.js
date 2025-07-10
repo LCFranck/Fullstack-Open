3609ce5044a88a14daf23d10001fd891c2ac1966
LCFranck <cassandra.franck@gmail.com>
2025-07-10 17:40:27 +0300

    4.5 actually done

--- part4/Blog_list/tests/listHelper.test.js
+++ part4/Blog_list/tests/listHelper.test.js
@@ -2,9 +2,66 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+//part 4.3
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+//part 4.4
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+})
+
+
+// part 4.5
+
+describe('total likes', () => {
+  const listWithManyBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b5q17f8',
+      title: 'Saakelikatten',
+      author: 'Joe Gas',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/katten.pdf',
+      likes: 50000,
+      __v: 0
+    },{
+      _id: 'aaqqqqqqqqqqqqqqqq',
+      title: 'Frog facts',
+      author: 'Man Upp',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/facts.pdf',
+      likes: 7,
+      __v: 0
+    }
+  ]
+  
+  test('this is stooopid', () => {
+    const result = listHelper.mostLikes(listWithManyBlog)
+    assert.deepStrictEqual(result, listWithManyBlog[1])
+  })
 })
--- part4/Blog_list/utils/list_helper.js
+++ part4/Blog_list/utils/list_helper.js
@@ -1,7 +1,26 @@
+
+
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy
+const totalLikes = (blogs) => {
+    console.log("blogs here!", blogs)
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
+const mostLikes = (blogs) => {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", blogs[1])
+    let best = blogs[0]
+    for (let i in blogs) {
+        if (best.likes<=blogs[i].likes){
+            best= blogs[i]
+        }
+    }
+    return best
+}
+
+module.exports = {
+  dummy, totalLikes, mostLikes
+}
+
