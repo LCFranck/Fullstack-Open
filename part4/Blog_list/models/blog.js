1b4dc6502134a0ffe5b86ad907199169f4a6d1dc
LCFranck <cassandra.franck@gmail.com>
2025-07-10 16:37:32 +0300

    4.1 done

---
+++ part4/Blog_list/models/blog.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+/* const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  important: Boolean,
+})
+
+*/
+
+
+
+//added
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: String,
+  url: String,
+  likes: { type: Number, default: 0 }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+}) 
+
+module.exports = mongoose.model('Blog', blogSchema)
