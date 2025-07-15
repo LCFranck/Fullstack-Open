cb7ad8c93c7210e4816b68b6def1e93c0b3b7f7f
LCFranck <cassandra.franck@gmail.com>
2025-07-15 16:48:59 +0300

    started on 4.15

--- part4/Blog_list/app.js
+++ part4/Blog_list/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -24,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
