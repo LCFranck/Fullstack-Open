5e23db567df7c7f88a7b4f5da9a3ec2425a198c7
LCFranck <cassandra.franck@gmail.com>
2025-07-17 14:29:48 +0300

    4.17 done

--- part4/Blog_list/controllers/blogs.js
+++ part4/Blog_list/controllers/blogs.js
@@ -13,16 +13,53 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.get('/', async (request, response) => {
-  const notes = await Blog
+  const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+/* 
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
+  const blog = new Blog({
+    content: body.content,
+    important: body.important || false,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+ */
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const user = await User.findById(body.user)
   if (!user) {   
-     return response.status(400).json({ error: 'userId missing or not valid' })  }
+    return response.status(400).json({ error: 'userId missing or not valid' })  
+     
+    }
 
   const blog = new Blog({
     title: body.title,
@@ -32,7 +69,8 @@ blogsRouter.post('/', async (request, response) => {
     user: user._id,
   })
 
-
+  
+  const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)  
   await user.save()
   response.status(201).json(savedBlog)
