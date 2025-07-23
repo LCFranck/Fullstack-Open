81c36c92b7ccf215a1fa9b5add920d8dd09c38e0
LCFranck <cassandra.franck@gmail.com>
2025-07-23 15:48:28 +0300

    5.1 done

--- part3/phonebook_p2/phonebook_frontend/src/services/persons.jsx
+++ part3/phonebook_p2/phonebook_frontend/src/services/persons.jsx
@@ -1,10 +1,6 @@
 import axios from 'axios'
 const baseUrl = '/api/persons'
-/*
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}*/
+
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data)
