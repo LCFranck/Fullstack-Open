2eb27d04c348bbd77ef9a88d4be42828d8ddcf8d
LCFranck <cassandra.franck@gmail.com>
2025-10-23 14:32:46 +0300

    6.15 done

--- part6/redux-anecdotes/src/components/AnecList.jsx
+++ part6/redux-anecdotes/src/components/AnecList.jsx
@@ -39,8 +39,8 @@ const AnecList = () => {
                     </div>
             
             )}
-        </ul>
-            )
-    }
+        </ul>)
+            
+  }
 
 export default AnecList
