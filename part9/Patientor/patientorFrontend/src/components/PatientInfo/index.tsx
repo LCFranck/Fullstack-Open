8d6a1bfb69dbd649e75f3f6c1ba019afdfbfee95
LCFranck <cassandra.franck@gmail.com>
2026-01-05 14:25:20 +0200

    9.25 done

--- part9/Patientor/patientorFrontend/src/components/PatientInfo/index.tsx
+++ part9/Patientor/patientorFrontend/src/components/PatientInfo/index.tsx
@@ -2,8 +2,10 @@
 
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Patient } from "../../types";
+import { Patient, Entry, Diagnosis } from "../../types";
 import patientService from "../../services/patients";
+import diagnosisService from "../../services/diagnoses";
+
 
 /* interface Props {
 
@@ -13,8 +15,9 @@ const PatientInfo = () => {
    
     const  id  = String(useParams().id);
 
-  const [patient, setPatient] = useState<Patient | null>(null);
+    const [patient, setPatient] = useState<Patient | null>(null);
 
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[] | null>(null);
 
 
  useEffect(() => {
@@ -22,11 +25,15 @@ const PatientInfo = () => {
         console.log("ojdå");
     }
 
-    const fetchPatient = async () => {
-      const data = await patientService.getPatient(id);
-      setPatient(data);
+    const fetchData = async () => {
+      const patientData = await patientService.getPatient(id);
+      setPatient(patientData);
+
+      const diagData = await diagnosisService.getAll();
+      setDiagnoses(diagData);
     };
-    void fetchPatient();
+    
+    void fetchData();
   }, [id]);
   
     console.log(patient);
@@ -34,13 +41,34 @@ const PatientInfo = () => {
     if (!patient) {
   return <div>Loading...</div>;
 }
-// i have a spelling error where i call ssh ssn, dont know where this started might fix it might not
 
+//helper function to find the corresponding diagnosis
+    const findDiagnosis = (code: string) => {
+        const diagnosis = diagnoses?.find(diag => diag.code === code);
+        return diagnosis?.name;
+    };
+
+// i have a spelling error where i call ssh ssn, dont know where this started might fix it might not
   return(
     <div>
         <h1>{patient.name}</h1>
-        <h3>ssh: {patient.ssn}</h3>
-        <h3>occupation: {patient.occupation}</h3>
+        <p>ssh: {patient.ssn}</p>
+        <p>occupation: {patient.occupation}</p>
+        <h2> entries </h2>
+        {patient.entries.map((entry: Entry) => (
+            <div key={entry.id}>
+            <p> {entry.date} {entry.description }</p>
+        
+            {entry.diagnosisCodes?.map((code: string) => (
+                <li key={code}> {code} {findDiagnosis(code)} </li>
+            ))}
+            
+            <li> {entry.type} </li>
+            </div>
+
+        ))}
+
+     
     </div>
     );
 
--- part9/Patientor/patientorFrontend/src/types.ts
+++ part9/Patientor/patientorFrontend/src/types.ts
@@ -10,6 +10,44 @@ export enum Gender {
   Other = "other"
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: number; //this could be the enum aswell? unsure if its needed to validate but on server and client
+}
+
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +55,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
-export type PatientFormValues = Omit<Patient, "id" | "entries">;
+export type PatientFormValues = Omit<Patient, "id" >;
